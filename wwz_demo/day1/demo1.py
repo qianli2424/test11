27249b3bdeab0da45457ac49c9ea290d0dba01dc
86137 <1427245437@qq.com>
2019-12-16 18:41:41 +0800

    1

--- wwz_demo/day1/demo1.py
+++ wwz_demo/day1/demo1.py
@@ -3,17 +3,18 @@
 # @Author:啊志
 # @Time:2019/9/23 9:20
 import random
-while True:
+n=1
+while n<4:
     num=random.randint(1,10)
     a=int(input("请输入一个数:"))
     if a==num:
         print("恭喜你猜对了,本次随机数为:{}".format(num))
-        break
+        # break
     else:
         print("猜错了,本次随机数为:{}".format(num))
-    b=input("是否继续游戏，按任意键继续，按n/N结束")
-    # if b=="n" or b=="N":
-    #     break
+    # b=input("是否继续游戏，按任意键继续，按n/N结束")
+    n += 1
+
 
 
 
