22795c77079ce4ba202510ee18e0eec681ef4fd2
86137 <1427245437@qq.com>
2019-12-16 16:09:30 +0800

    1

--- wwz_demo/day1/demo1.py
+++ wwz_demo/day1/demo1.py
@@ -4,6 +4,10 @@
 # @Time:2019/9/23 9:20
 import random
 num=random.randint(1,10)
-input("请输入一个数：")
-mmm
+a=input("请输入一个数：")
+if a==num:
+    print("恭喜你猜对了,本次随机数为:{}".format(num))
+else:
+    print("猜错了,本次随机数为:{}".format(num))
+
 
