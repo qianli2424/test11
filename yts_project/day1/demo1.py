169da218e617f4d1cd9006a11c650f32eb608c72
百依百顺 <1563179964@qq.com>
2019-12-16 16:22:28 +0800

    1

--- yts_project/day1/demo1.py
+++ yts_project/day1/demo1.py
@@ -1,13 +1,21 @@
 # _*_coding:utf-8 _*_
 #@Author:YTS
 #@文件说明：随机数大于5
-
 import random
+# import random
+#
+# num = random.randint(1,10)
+# if num>5:
+#     print('猜对了')
+#     print('猜对了随机数为：',num)
+# else:
+#     print('猜错了随机数为：',num)
 
-num = random.randint(1,10)
-if num>5:
+
+a=int(input('请随机输入一个数字:'))
+num = random.randint(1,3)
+if a==num:
     print('猜对了')
-    print('猜对了随机数为：',num)
+    print('猜对了随机数为：{}'.format(num))
 else:
     print('猜错了随机数为：',num)
-
