1d7a5c893924519ac7db247987d0b43466ad5b88
qianli2424 <2144543@qq.com>
2019-12-23 09:32:40 +0800

    1

--- qianliProject/day5/demo4.py
+++ qianliProject/day5/demo4.py
@@ -3,11 +3,27 @@
 # 文件说明：
 import openpyxl
 
-user = ['admin','123456','unlock',2]
-file = openpyxl.load_workbook('linux.xlsx')
-sheet = file['Sheet1']
-maxrow = sheet.max_row
+file = openpyxl.load_workbook('test.xlsx')
+sheet1 = file['Sheet1']
+maxrow = sheet1.max_row
+maxcol = sheet1.max_column
 
+users = []
+#得到n行数据
+for x in range(maxrow-1):
+    # 得到一行数据
+    user = []
+    for i in range(maxcol-2):
+        data = sheet1.cell(x+2, i+2).value
+        user.append(data)
+    #print(user)     #user是一行内容
+    users.append(user)  #我是把一整行内容放到列表中
+print(users)
 
-for i in range(len(user)):
-    sheet.cell(maxrow+1,i+1).value = user[i]
+name = input('请输入用户名')
+password = input('请输入密码')
+user = [name,password,'unlock']
+if user in users:
+    print('登录成功')
+else:
+    print('登录失败')
---
+++ qianliProject/day5/demo5.py
@@ -0,0 +1,8 @@
+# -*- coding=utf8 -*-
+#@author:qianli
+# 文件说明：
+a = ''
+if '用户名和密码都正确':
+    print('登录成功')
+elif '用户名或密码或状态不正确':
+    print('登录失败')
